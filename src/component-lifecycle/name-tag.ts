e1716009ddfc789eb653d4fbc8074aa8580c89ab
EisenbergEffect <rob@bluespire.com>
2017-06-20 21:33:30 -0700

    feat(demos): working on more demo code

---
+++ src/component-lifecycle/name-tag.ts
@@ -0,0 +1,26 @@
+import {bindable} from 'aurelia-templating';
+
+export class NameTag {
+  @bindable name;
+  @bindable color;
+
+  created() {
+    
+  }
+
+  bind() {
+
+  }
+
+  attached() {
+
+  }
+
+  detached() {
+
+  }
+
+  unbind() {
+
+  }
+}
