efb410d735e3964e4773a925a0f9175e3fc72db2
EisenbergEffect <rob@bluespire.com>
2017-06-23 23:12:16 -0700

    feat(app): add pageup/pagedown navigation

--- src/app/app.ts
+++ src/app/app.ts
@@ -18,4 +18,30 @@ export class App {
       docElm.webkitRequestFullScreen();
     }
   }
+
+  activate() {
+    document.addEventListener('keyup', evt => {
+      if (evt.keyCode === 34) {
+        this.next();
+      } else if (evt.keyCode == 33) {
+        this.previous();
+      }
+    });
+  }
+
+  next() {
+    let index = this.router.routes.indexOf(this.router.currentInstruction.config);
+
+    if ((index + 1) < this.router.routes.length) {
+      this.router.navigate(this.router.routes[index + 1].navModel.href);
+    }
+  }
+
+  previous() {
+    let index = this.router.routes.indexOf(this.router.currentInstruction.config);
+
+    if (index > 0) {
+      this.router.navigate(this.router.routes[index - 1].navModel.href);
+    }
+  }
 }
