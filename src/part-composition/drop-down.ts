7e85167002de6937290cc0872ddd058d47a303e7
EisenbergEffect <rob@bluespire.com>
2017-06-24 21:41:24 -0700

    feat(drop-down): better template part html

--- src/part-composition/drop-down.ts
+++ src/part-composition/drop-down.ts
@@ -1,9 +1,11 @@
-import {bindable} from 'aurelia-templating';
+import {bindable, processContent} from 'aurelia-templating';
 import {bindingMode} from 'aurelia-binding';
 import {TaskQueue} from 'aurelia-task-queue';
 import {autoinject} from 'aurelia-dependency-injection'; 
+import {DOM, FEATURE} from 'aurelia-pal';
 
 @autoinject
+@processContent(childrenAsTemplateParts('selected-item-template', 'list-item-template'))
 export class DropDown {
   @bindable items;
   @bindable displayExpression = '$this';
@@ -26,5 +28,33 @@ export class DropDown {
       this.listIsOpen = true;
       this.taskQueue.queueTask(() => document.addEventListener('click', this.toggleList));
     }
+  }
+}
+
+function childrenAsTemplateParts(...partNames: string[]) {
+  return function convertChildElementsToTemplateParts(compiler, resources, node: HTMLElement, instruction) {
+    let currentChild = node.firstElementChild;
+
+    while (currentChild) {
+      let nextSibling = currentChild.nextElementSibling;
+      let elementName = currentChild.tagName.toLowerCase();
+
+      if (partNames.indexOf(elementName) !== -1) {
+        let template = <HTMLTemplateElement>DOM.createElement('template');
+        (<any>FEATURE).ensureHTMLTemplateElement(template);
+        let content = template.content;
+
+        while(currentChild.firstChild) {
+          content.appendChild(currentChild.firstChild);
+        }
+
+        template.setAttribute('replace-part', elementName);
+        DOM.replaceNode(template, currentChild, node);
+      }
+
+      currentChild = nextSibling;
+    }
+
+    return true;
   }
 }
